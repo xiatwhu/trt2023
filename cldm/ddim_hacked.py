2e5444eb27bd4e46347252b0734afc6987f8ae10
xiatwhu <xiatwhu@live.com>
2023-07-24 06:34:15 +0800

    test 15

--- cldm/ddim_hacked.py
+++ cldm/ddim_hacked.py
@@ -284,7 +284,7 @@ class DDIMSampler(object):
                     self.tensors[2].copy_(t_emb)
                     torch.cuda.synchronize()
 
-                    if index < 18:
+                    if index < 15:
                         cudart.cudaGraphLaunch(self.cuda_graph_instance, self.stream)
                         cudart.cudaStreamSynchronize(self.stream)
                     else:

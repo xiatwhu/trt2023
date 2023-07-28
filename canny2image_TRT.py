f47f2ca12caba8a2fa036c2baa4db5192f479e94
xiatwhu <xiatwhu@live.com>
2023-07-28 21:51:51 +0800

    other index list

--- canny2image_TRT.py
+++ canny2image_TRT.py
@@ -190,8 +190,8 @@ class hackathon():
                 # if index > 10:
                 # if index > 12 or index % 3 == 0:
                 # if index > 16 or index % 2 == 0:
-                # if index in [19, 18, 17, 16, 15, 14, 13, 12, 9, 6, 3, 0]: # 4.993117427548901
-                if index in [19, 18, 17, 16, 15, 14, 13, 11, 9, 6, 3, 0]: # 4.823739027404583
+                if index in [19, 18, 17, 16, 15, 14, 13, 12, 9, 6, 3, 0]: # 4.993117427548901
+                # if index in [19, 18, 17, 16, 15, 14, 13, 11, 9, 6, 3, 0]: # 4.823739027404583
                 # if index in [19, 18, 17, 16, 15, 14, 13, 11, 8, 5, 3, 0]:
                     cudart.cudaGraphLaunch(self.control_fp16_graph_instance, self.stream1)
                     cudart.cudaEventRecord(self.event1, self.stream1)
